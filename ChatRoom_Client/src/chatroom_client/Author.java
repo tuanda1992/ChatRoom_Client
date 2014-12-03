3bbc5c56c55ee61fa3e71252ef1b6b381a721c7c
Ut <Ut@thuc>
2014-12-04 06:02:06 +0700

    thêm hàm cho Chatclient_process

---
+++ ChatRoom_Client/src/chatroom_client/Author.java
@@ -0,0 +1,85 @@
+
+package chatroom_client;
+
+public class Author extends javax.swing.JFrame {
+
+    public Author() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Nhóm sinh viên thực hiện");
+
+        jLabel2.setText("Trần Bá Ý");
+
+        jLabel3.setText("Nguyễn Thanh Tùng");
+
+        jLabel4.setText("Lê Đăng Thức");
+
+        jLabel5.setText("Đoàn Anh Tuấn");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(44, 44, 44)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(103, 103, 103)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(49, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(jLabel2)
+                .addGap(31, 31, 31)
+                .addComponent(jLabel3)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel4)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel5)
+                .addContainerGap(46, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Author().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    // End of variables declaration//GEN-END:variables
+}
--- ChatRoom_Client/src/chatroom_client/ChatClient_Process.java
+++ ChatRoom_Client/src/chatroom_client/ChatClient_Process.java
@@ -1,10 +1,61 @@
 package chatroom_client;
 
-public class ChatClient_Process implements Setting,Runnable {
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+public class ChatClient_Process implements Setting,Runnable {
+    
+    public ChatClient_Process(javax.swing.JTextPane mPane,javax.swing.JList mList,ArrayList mArr)
+    {
+
     }
-  
+    
+Thread thread;
+public void run(){
+
 }
+public void AddMessageToPanel(String msg,String bgImageURL) {
+        
+        messageArr.add(msg);
+        strBMsg = new StringBuilder("<html><head></head><body background = \"" + bgImageURL +"\">");
+        
+        for(Object str: messageArr)
+        {
+            strBMsg.append(str.toString() + "<br/>");
+        }
+        strBMsg.append("</body></html>");
+        MessagePane.setText(strBMsg.toString());
+    }
+
+    public void SetBackgroundPanel(String bgImageURL)
+    {
+      
+        strBMsg = new StringBuilder("<html><head></head><body background = \"" + bgImageURL + "\">");
+        for(Object str: messageArr)
+        {
+            
+            strBMsg.append(str.toString() + "<br/>");
+            
+        }
+        strBMsg.append("</body></html>");
+        MessagePane.setText(strBMsg.toString());
+        
+    }
+     JTextPane MessagePane;
+    JList UserList;
+    ArrayList messageArr;
+    StringBuilder strBMsg;
+    DataOutputStream out;
+    int index1,index2,IconIndex;
+    String ImageDir;
+}
+
+    
+
