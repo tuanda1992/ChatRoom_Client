bdb5ed350192d6fa58517acd324d27a3e255ede1
Tran Ba Y <tranbay.tranbay@gmail.com>
2014-12-08 10:11:53 +0700

    Sửa lớp Setting (phần RoomName)

--- ChatRoom_Client/src/chatroom_client/Setting.java
+++ ChatRoom_Client/src/chatroom_client/Setting.java
@@ -7,6 +7,7 @@ public interface Setting {
     //mảng các smile
     public String[] arrSmiles = {":P",":)]",":-c",":-h",":-t","8->","x_x",":-*",":-q","=((",":-o",":>","B-)",":-S",":((",":))",":(",":)","=))","O:-)",";)","(:|",":O)",":-$","[-(",":ar!","I-)",":-w","8-}",":D"};
     public String imageFolderURL = "file:///Images\\";
-    public String[] roomList =  {"Phòng gà - Chicken","Phòng Xì - tai","Phòng Xí - muội","Phòng \"Bà tám\"...","Phòng V.I.P","Phòng IT"};
+    public String[] roomList =  {"Phòng Nhân sự","Phòng Tài Chính",
+        "Phòng ý tưởng","Phòng Thiet kế" ,"Phòng Đồ họa","Phòng IT"};
 
 }
